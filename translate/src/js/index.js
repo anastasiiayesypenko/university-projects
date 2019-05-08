1d7d63a33c96889e98ce9183c67aae4ba8bb29dc
Anastasia <nastiyayesypenko@ukr.net>
2019-05-08 19:46:51 +0300

    change json

--- translate/src/js/index.js
+++ translate/src/js/index.js
@@ -8,7 +8,21 @@ class Translator {
     onSelection(event) {
         let selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
         this.resultWindow.textContent = `You selected: ${selection}`;
+        const wordEndWhitespace = /\s$/gi;
+        if (wordEndWhitespace.test(selection)) {
+            selection.replace(' ', '');
+        }
       }
+    findWordInDictionary() {
+        const wordFetch = fetch('https://raw.githubusercontent.com/anastasiiayesypenko/university-projects/master/translate/src/js/dict.json')
+        .then(response => {
+            if (response.ok) return response.json();
+            throw new Error(err => console.log(response.statusText));
+        })
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+    }
 }
 let translatorPage = new Translator();
+translatorPage.findWordInDictionary();
 
